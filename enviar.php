5b97b9f1172ecfdc3fd09a215e363005363ec096
GlobalElite <71672839+globalelite-g5@users.noreply.github.com>
2020-10-21 07:38:05 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -5,6 +5,11 @@
     $nombre = $_POST['nombre'];
     $email = $_POST['email'];
     $contenido ="Nombre: " . $nombre . "\nCorreo: " . $email . "\nConsulta: " . $consulta;
-    mail($destino,"Contacto", $contenido);
-    header("Location:index2.html");
+
+    $header = "Enviado desde Unity web";
+    $mensajeCompleto = $mensaje . "/nAtentamente: " . $nombre;
+
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
+    echo "<scrip>alert('correo enviado exitosamente')</script>";
+    echo "<scrip> setTimeout(\"location.href='index.html'\",1000)</script>";
 ?>
